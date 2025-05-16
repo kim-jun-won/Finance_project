a2165db0d6671f61eab9f18a513c7b28e36c12d6
kim-jun-won <junwon2631@gmail.com>
2025-05-16 18:36:48 +0900

    Initial commit

---
+++ Finacne_News/src/main/java/com/example/Finacne_News/FinacneNewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Finacne_News;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinacneNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinacneNewsApplication.class, args);
+	}
+
+}
---
+++ Finacne_News/src/test/java/com/example/Finacne_News/FinacneNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Finacne_News;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinacneNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
